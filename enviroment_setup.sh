f3378f213efc8a608cf29e2050caad93ad0fccf5
unknown <cmpss92140@gmail.com>
2018-06-30 20:31:13 +0800

    new file:   enviroment_setup.sh

---
+++ enviroment_setup.sh
@@ -0,0 +1,8 @@
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x ./Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh
+source ~/miniconda3/bin/activate
+conda install pip
+pip install pygame
+pip install pytmx
+unzip ./mapeditor/tiled-linux-64bit-snapshot.zip 

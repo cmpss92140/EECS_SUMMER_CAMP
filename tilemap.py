7e74f9d8135f6d18b3716cf24e3c77d282389bbc
Angusky <angustke@gmail.com>
2018-06-30 11:54:39 -0700

    Fix game code

--- tilemap.py
+++ tilemap.py
@@ -27,17 +27,26 @@ class TiledMap:
         self.render(temp_surface)
         return temp_surface
 
-
-'''移動視窗 Camera'''
 class Camera:
-    def __init__(self):
-        pass
+    def __init__(self, width, height):
+        self.camera = pg.Rect(0, 0, width, height)
+        self.width = width
+        self.height = height
+        self.pos = vec(0,0)
 
     def apply(self, entity):
-        pass
+        return entity.rect.move(self.camera.topleft)
 
-    def apply_rect(self, entity):
-        pass
+    def apply_rect(self, rect):
+        return rect.move(self.camera.topleft)
 
     def update(self, target):
-        pass
+        x = -target.rect.centerx + int(WIDTH / 2)
+        y = -target.rect.centery + int(HEIGHT / 2)
+
+        x = min(0, x)  
+        y = min(0, y)  
+        x = max(-(self.width - WIDTH), x)  
+        y = max(-(self.height - HEIGHT), y)  
+        self.pos = vec(x,y)
+        self.camera = pg.Rect(x, y, self.width, self.height)
